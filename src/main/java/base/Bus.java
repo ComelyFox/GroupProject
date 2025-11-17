bb5fdf31d7267e6878c128e94e752f4fa79767ea
evgen <evgeniy.osin.87@mail.ru>
2025-11-17 22:51:06 +0300

    - Добавлена заглушка класса Bus для разработки сортировки.Будет заменена на реальный класс.
    - Добавлен общий интерфейс SortingStrategy.
    
    - Добавлен абстрактный класс QuickSortStrategy c реализацией быстрой сортировки.

---
+++ src/main/java/base/Bus.java
@@ -0,0 +1,20 @@
+package base;
+
+// Заглушка для реализации сортировки. Необходимо заменить на реальный класс
+public class Bus {
+    private String model;
+    private int SerialNumber;
+    private int mileage;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSerialNumber() {
+        return SerialNumber;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+}
