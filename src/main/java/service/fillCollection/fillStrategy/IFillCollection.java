16c7e811b105058e078f6a3fae43d527186508e1
MaxTsumentsau <max2ba@yandex.ru>
2025-11-26 15:48:03 +0300

    Вариант программы с полностью реализованным функционалом согласно задания (сортировки по полям не удалял)

--- src/main/java/service/fillCollection/fillStrategy/IFillCollection.java
+++
@@ -1,8 +0,0 @@
-package service.fillCollection.fillStrategy;
-
-import java.util.HashMap;
-import java.util.List;
-
-public interface IFillCollection {
-    List<HashMap<String, Object>> getFillCollection();
-}
--- src/main/java/service/fillCollection/fillStrategy/random/parts/IRandomPart.java
+++
@@ -1,5 +0,0 @@
-package service.fillCollection.fillStrategy.random.parts;
-
-public interface IRandomPart {
-    StringBuilder randomPart(int length);
-}
